fc779834d327880f32518b153cf543fe95c69751
r3st <rene@r3st.de>
2021-01-30 15:15:25 +0100

    fix copy/paste second misstake

--- scripts/createBucki_exporter.bash
+++ scripts/createBucki_exporter.bash
@@ -6,4 +6,4 @@ git clone https://github.com/r3st/bucki_exporter.git
 
 cd /tmp/bucki_exporter
 
-docker build -f docker/Dockerfile -t docker build -f docker/Dockerfile -t test .
+docker build -f docker/Dockerfile -t r3st/bucki_exporter:0.0.1 .
